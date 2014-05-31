be3b6e2f4dbf44f25f77897b9f940e5674770684
alosada <alosada@gmail.com>
2014-05-31 16:54:02 -0700

    Added player and game objects   * PLAYER     * init with key and position = 0     * added advancePlayer functionality   * GAME     * init with player array, tranck length     * added getKeyCode, startGame, bindEventListeners functionality

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
 window.addEventListener( 'load', initialize)
 
 function initialize(){
-  console.log("hji guys")
+  console.log("initialize printed this")
+  game = new Game(10)
+  game.bindEventListeners()
 }
 
-function game(){}
+function Game(length){
+  this.players = []
+  this.trackLength = length
+  console.log("game printed this")
+}
+
+function Player(){
+  console.log("player printed this")
+  this.position =  0;
+  this.key = 66;
+}
+
+Player.prototype = {
+  updatePosition: function(){
+    console.log("updated pos printed this.")
+    this.position ++
+  }
+}
+
+Game.prototype = {
+  bindEventListeners: function(){
+    document.addEventListener('keyup', this.getKeyCode);
+    document.getElementById("player-number-form").addEventListener('submit', this.startGame);
+  },
+  getKeyCode: function(e){
+    return e.keyCode
+  },
+  startGame: function(e) {
+   e.preventDefault()
+   var numberOfPlayers = document.getElementById("player-number").value
+   this.initPlayers(numberOfPlayers)
+  },
+  initPlayers: function(numberOfPlayers) {
+
+  }
+}
