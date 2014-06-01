202e46c66412166766218419f1906fa091bcfa3d
alosada <alosada@gmail.com>
2014-05-31 17:33:26 -0700

    New player functionality.   * Created key-selector in the DOM   * Added functionality to create players with their key of choice     * Added event listener     * Added function to create player

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function initialize(){
 
 function Game(length){
   this.players = []
+  this.keys = []
   this.trackLength = length
   console.log("game printed this")
 }
@@ -29,16 +30,25 @@ Game.prototype = {
   bindEventListeners: function(){
     document.addEventListener('keyup', this.getKeyCode);
     document.getElementById("player-number-form").addEventListener('submit', this.startGame);
+    document.getElementById("keys").addEventListener('click', this.newPlayer);
   },
+
   getKeyCode: function(e){
     return e.keyCode
   },
+
   startGame: function(e) {
    e.preventDefault()
    var numberOfPlayers = document.getElementById("player-number").value
-   this.initPlayers(numberOfPlayers)
   },
-  initPlayers: function(numberOfPlayers) {
 
-  }
+  initPlayers: function(numberOfPlayers) {
+    // for(var i=0; i<numberOfPlayers; i++){
+    //   debugger
+    //   this.players.push()
+    // }
+  },
+    newPlayer: function(){
+      return new Player(event.target.innerHTML)
+   }
 }
